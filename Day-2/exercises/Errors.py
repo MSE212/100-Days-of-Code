4be6056c4096197c8723d540e5d478a47c8dc59b
MSE212 <franciscoslzr01@gmail.com>
2026-06-09 12:17:49 -0600

    Types of Errors Lesson

---
+++ Day-2/exercises/Errors.py
@@ -0,0 +1,24 @@
+# Type Error
+#len(12345)
+
+print(len("Haro Evrynyan"))
+
+print(type("Hello"))
+print(type(212))
+print(type(21.2))
+print(type(True))
+
+# Type conversion or Type casting
+print("123" + "456")
+print(int("123"))
+print(int("123") + int("456"))
+
+int()
+float()
+str()
+bool()
+
+user_name = input("What is your name? ")
+name_length = len(user_name)
+
+print("Number of letters in your name: " + str(name_length))

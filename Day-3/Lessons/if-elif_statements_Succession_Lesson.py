add64637d77dab63fc2eca33aaedf9c97e95deaf
MSE212 <franciscoslzr01@gmail.com>
2026-06-14 12:13:12 -0600

    photo and total bill added

---
+++ Day-3/Lessons/if-elif_statements_Succession_Lesson.py
@@ -0,0 +1,29 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+bill = 0
+
+if height >= 120:
+    print("You can ride the rollercoaster.")
+
+    age = int(input("What is your age? "))
+    
+    if age < 12:
+        print("Please pay $5")
+        bill = 5
+    elif age <= 18:
+        print("Please pay $7")
+        bill = 7
+    else:
+        print("Please pay $12")
+        bill = 12
+
+    photos = input("Do you want photos? Y/N\n")
+    
+    if photos == 'Y':
+        bill += 3
+        print(f"The total bill is ${bill}")
+    else:
+        print(f"Your total bill is ${bill}")
+
+else:
+    print("Sorry you have to grow taller before you can ride.")
